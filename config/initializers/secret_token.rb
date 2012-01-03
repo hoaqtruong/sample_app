d22ea2bdbf13647f1cfbe5413a872d161a333f31
Hoa <tq_hoa1977@yahoo.com>
2012-01-03 11:21:42 -0800

    Initial commit. Could not install 'rails generate rspec:install'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a8cba97e8c200e635c56dddc9dcfacdaa48c5b2c4e61439a9f35c0e287348e365c2486624c52224a00a4cea5701747fcb41b9191877ca880298d8cee9a3bd3f6'
