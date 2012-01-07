3677b2ed487be5f5c0e96cdfb87f364f1ffccc13
Hoa <tq_hoa1977@yahoo.com>
2012-01-07 12:52:45 -0800

    Title_refactored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis
+  def title
+    base_title = "RoR Sample App"
+    if @title.nil?
+      base title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
