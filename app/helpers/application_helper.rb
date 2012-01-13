f7b5e5772953a6fc701aa1fa169d20c97ccec67d
Hoa <tq_hoa1977@yahoo.com>
2012-01-13 13:13:39 -0800

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   
+  #return logo
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample RoR App", :class => "round") 
+  end
+  
   # Return a title on a per-page basis
   def title
     base_title = "RoR Sample App"
     if @title.nil?
-      base title
+      base_title
     else
       "#{base_title} | #{@title}"
     end
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  before(:each) do
+    @base_title = "RoR Sample App"
+  end
+ 
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => @base_title + " | Home")
+  end
+  
+  it "should have a Contact page at '/contact' " do
+    get '/contact'
+    response.should have_selector('title', :content => @base_title + " | Contact")
+  end
+  
+  it "should have an About page at '/about' " do
+    get '/about'
+    response.should have_selector('title', :content => @base_title + " | About")
+  end
+  
+  it "should have a Help page at '/help' " do
+    get '/help'
+    response.should have_selector('title', :content => @base_title + " | Help")
+  end
+  
+  it "should have a Sign up page at '/signup' " do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
+  # it "should have the right links on the layout" do
+  #   visit root_path
+  #   click_link "About"
+  #   response.should have_selector('title', :content => "About")
+  #   click_link "Help"
+  #   response.should have_selector('title', :content => "Help")
+  #   click_link "Contact"
+  #   response.should have_selector('contact', :content => "Contact")
+  #   click_link "Home"
+  #   response.should have_selector('title', :content => "Home")
+  #   click_link "Sign up now!"
+  #   response.should have_selector('contact', :content => "Sign up now")
+  # end
+     
+end
